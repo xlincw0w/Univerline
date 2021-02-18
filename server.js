6e54d4bdadae9ab38be7ab80146ddf9eb35af83e
xlincw0w <khaled.khazem@yahoo.com>
2021-02-18 14:57:10 +0100

    restapi int + online database

--- server.js
+++ server.js
@@ -1,26 +1,63 @@
 const express = require('express')
+const cors = require('cors')
+const knex = require('knex')
 const path = require('path')
+const PORT = process.env.PORT || 3001
 
-// const webpack = require('webpack');
-// const webpackDevMiddleware = require('webpack-dev-middleware');
+require('dotenv').config()
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        ssl: { rejectUnauthorized: false },
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
+})
 
 const app = express()
-// const config = require('./webpack.config.js');
-// const compiler = webpack(config);
+
+app.use(express.json())
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: config.output.publicPath,
-// }));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+app.get('/api/connected', (req, res) => {
+    res.json({ connected_to_api: true })
 })
 
-// Serve the files on port 3000.
-app.listen(3000, function () {
-    console.log('Server listenning on port 3000!\n')
+app.get('/api/get/users', (req, res) => {
+    db('users')
+        .select('*')
+        .then((rows) => {
+            console.log(rows)
+            res.json(rows)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.post('/api/add/user', (req, res) => {
+    const data = req.body
+
+    db('users')
+        .insert({
+            user_id: data.user_id,
+            username: data.username,
+        })
+        .then((response) => {
+            console.log(response)
+            res.json({ updated: true })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ updated: false })
+        })
+})
+
+app.listen(PORT, function () {
+    console.log(`Server listenning on port ${PORT}!\n`)
 })
