27d7aacc5c9a7ac9e2d3c4ae8f1dc089fca8bf1b
naimbessaha4 <78882432+naimbessaha4@users.noreply.github.com>
2021-03-01 11:51:54 +0100

    fix

--- src/Layout/Inscription/inscription.js
+++ src/Layout/Inscription/inscription.js
@@ -1,26 +1,9 @@
-import React, {useState, useEffect} from 'react'
-import '../Inscription/nav.css'
+import React from 'react'
 
 export default function Inscription() {
-    const [nav, setnav]=useState(true);
-    const btnnav =() => {
-        setnav(!nav);
-    }
     return (
-        <nav>
-        
-            {nav && (
-                
-
-            <ul className="liste">
-            <li className="items">Accueil</li>
-            <li className="items">Service</li>
-            <li className="items">Contact</li>
-            </ul>
-            
-        )}
-            
-            <button className="btn" onClick={btnnav} >BTN</button>
-        </nav>
+        <div>
+            <p>Formulaire d'inscription</p>
+        </div>
     )
 }
