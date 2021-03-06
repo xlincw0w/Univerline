319568c66ccfb80da2f2ed544857f535775fe3dd
naimbessaha4 <78882432+naimbessaha4@users.noreply.github.com>
2021-03-06 21:21:23 +0100

    messagerie

--- src/Layout/Main/main.js
+++ src/Layout/Main/main.js
@@ -26,6 +26,7 @@ const Main = () => {
                             <Profile />
                         </Route>
                         <Route path='/messagerie'>
+                            <Header />
                             <Messagerie/>
                         </Route>
                         <Route path='/auth'>
