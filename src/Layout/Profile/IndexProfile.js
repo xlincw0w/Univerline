b67d08fb006562dce053858fa8233880bef54290
cheick98 <78909708+cheick98@users.noreply.github.com>
2021-03-11 12:46:02 +0100

    'profile

--- src/Layout/Profile/IndexProfile.js
+++ src/Layout/Profile/IndexProfile.js
@@ -4,58 +4,60 @@ import Menu from './ProfileEtudiant/Component/Menu'
 import ProfileEtudiant from './ProfileEtudiant/ProfileEtudiant'
 
 class IndexProfile extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            choix: '',
-            choixs: ['', 'Profile Etudiant', 'Profile Enseignant'],
-        }
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      choix: "",
+      choixs: ["", "Profile Etudiant", "Profile Enseignant"]
     }
+  }
+  
+  choisir = (e) =>{
+    this.setState({
+      choix: e.target.value,
+    })
+  }
 
-    choisir = (e) => {
-        this.setState({
-            choix: e.target.value,
-        })
-    }
+  render() {
 
-    render() {
-        //  const choixFait = this.state.choix == "Profile Etudiant" ? (
-        //    <ProfileEtudiant />
-        //  ) : (
-        //    this.state.choix == "Profile Enseignant" ? (
-        //      <Profile />
-        //    ) : ("")
-        //    )
+    const choixFait = this.state.choix == "Profile Etudiant" ? (
+      <ProfileEtudiant />
+    ) : (
+      this.state.choix == "Profile Enseignant" ? (
+        <Profile />
+      ) : ("")
+      )
+    
+    return (
+      <div>
 
-        return (
-            <div style={{ overflow: 'hidden' }}>
-                {/* <p>
-                    <label for='choix'>Choisir le profile à afficher : </label>
-                    <select id='choix' value={this.state.choix} onChange={this.choisir}>
-                        {this.state.choixs.map((choix, index) => {
-                            return (
-                                <option key={index} value={choix}>
-                                    {choix}
-                                </option>
-                            )
-                        })}
-                    </select>
-                </p>  */}
+        <p>
+          <label for="choix">Choisir le profile à afficher : </label>
+          <select id="choix" value={this.state.choix} onChange={this.choisir} >
+            {
+              this.state.choixs.map((choix, index) =>{
+                return <option key={index} value={choix} > {choix} </option>
+              } ) 
+            }
+          </select>
+          </p>
 
-                {/* {choixFait} */}
-                <br />
-                <ProfileEtudiant />
-                {/*<Menu />
+        {choixFait}
+        <br />
+        {/* <ProfileEtudiant /> */}
+        {/*<Menu />
         
 
         <p>{this.state.choix}</p>
         <br />
         <ProfileEtudiant />
         <br />*/}
-            </div>
-        )
-    }
+            
+      </div>
+    )
+  }
 }
 
 export default IndexProfile
