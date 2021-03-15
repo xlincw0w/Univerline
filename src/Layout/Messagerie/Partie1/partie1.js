a35148a3651464ec56986cbb6957fa67e5f41a0a
naimbessaha4 <78882432+naimbessaha4@users.noreply.github.com>
2021-03-15 20:29:21 +0100

    messageriePartie1

--- src/Layout/Messagerie/Partie1/partie1.js
+++ src/Layout/Messagerie/Partie1/partie1.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
-import Users from './users'
+import React, { useState, useEffect} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import { useSelector,useDispatch  } from 'react-redux'
+import  { constants } from '../../../constants'
+import { SetFriends } from '../../../store/auth/auth'
+import Axios from 'axios'
 import img from '../img/1.jpg'
 import img2 from '../img/2.jpg'
 import img3 from '../img/3.jpg'
 import img4 from '../img/4.jpg'
 import Grid from '@material-ui/core/Grid'
 import Avatar from '@material-ui/core/Avatar'
+
 import './partie1.css'
+import Users from './users'
+
+
+
 
 const useStyles = makeStyles((theme) => ({
-    
+
 
     large: {
         width: theme.spacing(7),
@@ -21,10 +29,31 @@ const useStyles = makeStyles((theme) => ({
 
 export default function partie1(props) {
     const {onclick2}=props;
+     
+
     const [searchUser, setsearchUser]= useState('')
+    const dispatch = useDispatch()
+    const user = useSelector((state) => state.AuthReducer.user)
+    const friends = useSelector((state) => state.AuthReducer.friends)
+    
+    
 
+    useEffect(() => {
+        
+            Axios.get(constants.url + '/api/amis/get/amis/' + user.id)
+                .then((res) => {
+                    dispatch(SetFriends(res.data))
+                }).catch((err) => {
+                    dispatch(SetFriends([]))
+                })
+                
+        
+    }, [user.id])
 
-    const users = {
+   
+   
+   
+const users = {
         utilisateurs: [
             {
                 FirstName: 'Naim',
@@ -88,15 +117,17 @@ export default function partie1(props) {
     }
 
     const classes = useStyles()
+    const lent = friends.length
+    
   
 
     
 
     return (
-        <Grid container  xs={3} style={{ height: '89vh' ,borderRight:'1px solid rgb(202, 202, 202)'}}>
-        <Grid container className='' xs={12}  style={{ height: '30%',borderBottom:'1px solid rgb(202, 202, 202)', borderRadius:'10px' }}>
+        <Grid container  xs={3} style={{ height: '89vh' ,borderRight:'1px solid rgb(202, 202, 202)'}} >
+        <Grid container className='' xs={12}  style={{ height: '23%',borderBottom:'1px solid rgb(202, 202, 202)', borderRadius:'10px' }}>
             <Grid container xs={12}>
-                <Grid className='myAvatar'>
+                <Grid className='myAvatar bg-gradient-to-r from-white to-blue-600'>
                     <Avatar alt='Reseau social' src={img} className={classes.large} />
                     <h1>Ma Messagerie</h1>
                 </Grid>
@@ -109,18 +140,21 @@ export default function partie1(props) {
                 </Grid>
             </Grid>
         </Grid>
-        <Grid xs={12} className='over' style={{ height: '70%' }}>
-            {users.utilisateurs.filter((user) => {
+        <Grid xs={12} className='over' style={{ height: '77%' }}>
+            {  lent != 0 ?
+            friends.filter((user) => {
                 if (searchUser === ''){
                     return user
-                }else if (user.FirstName.toLowerCase().includes(searchUser.toLowerCase()) || user.LastName.toLowerCase().includes(searchUser.toLowerCase())){
+                }else if (user.prenom.toLowerCase().includes(searchUser.toLowerCase()) || user.nom.toLowerCase().includes(searchUser.toLowerCase())){
                     return user
                 }
             }
 
-            ).map((user) => (
-                <Users user={user} onclick={onclick2} />
-            ))}
+            ).map(user => (
+                
+                 <Users user={user} onclick={onclick2} />
+                
+                 )) : <h1 style={{textAlign:'center'}}>aucun ami disponible</h1>}
         </Grid>
     </Grid>
     )
--- src/Layout/Messagerie/Partie1/users.js
+++ src/Layout/Messagerie/Partie1/users.js
@@ -27,15 +27,15 @@ export default function Users(props) {
         
         
         
-        <Grid  className="flex" xs={12}  className="util outline-none focus:outline-none select-text" onClick={() => onclick(user)}>
+        <Grid  className="flex" xs={12}  className="util outline-none focus:outline-none select-text bg-gray-200 text-gray-800" onClick={() => onclick(user)}>
              
            
             <Grid className="flex" >
         
-              <Avatar alt="Reseau social" src={user.Image} className={classes.large} />
+              <Avatar alt={user.prenom} src={user.avatar} className={classes.large} />
               <Grid className='userdata' style={{ marginLeft: "10px" }} >
-                <p>{user.FirstName} {user.LastName}</p>
-                <p>{user.Statu}</p>
+                <p>{user.prenom} {user.nom}</p>
+                <p>{user.user_type}</p>
               </Grid>
               </Grid>
         </Grid>
