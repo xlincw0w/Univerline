fd22e3848479732e8f20cf68eb10118a534ef59a
koceyla2702 <45518884+koceyla2702@users.noreply.github.com>
2021-03-17 14:34:18 +0100

    messagerie

--- src/Layout/Messagerie/Partie2/partie2.js
+++ src/Layout/Messagerie/Partie2/partie2.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
-import Message from './messages'
+import Message from './msg'
 import Grid from '@material-ui/core/Grid'
 import SendIcon from '@material-ui/icons/Send'
 import ListIcon from '@material-ui/icons/List'
 import './partie2.css'
+import Champ from './champ'
 
 const useStyles = makeStyles((theme) => ({
     xlarge: {
@@ -32,12 +33,11 @@ export default function partie2(props) {
     }
 
     return (
-        <Grid container className='grid-flow-row auto-rows-max ' xs={alo} style={{ borderRight: '1px solid rgb(202, 202, 202)' }}>
-            {/* 
-entete
-*/}
-            <Grid container className='flex shadow-lg' style={{ height: '15%' }} xs={12}>
-                <Grid xs={9} className='entetinformation flex'>
+        <Grid container className='grid-flow-row auto-rows-max pt-1' xs={alo} style={{ height: '100%', borderRight: '2px solid rgb(202, 202, 202)' }}>
+            {/*  entete */}
+
+            {/* <Grid container className='flex shadow-lg gap-y-2 rounded-2xl bg-gray-100  p-1' style={{ height: '5%' }} xs={12}>
+                 <Grid xs={9} className='entetinformation flex'>
                     <Avatar alt='Neseau social' src={Image} className={classes.xlarge} />
                     <div className='block infpersonel'>
                         <p> {FirstName} </p>
@@ -47,30 +47,19 @@ entete
 
                 <Grid xs={2} style={{ textAlign: 'right', marginTop: '20px' }}>
                     <ListIcon inputProps={{ 'aria-label': 'primary checkbox' }} onClick={div} color='primary' checked={switch1} />
-                </Grid>
-            </Grid>
-            {/* 
- messages
+                </Grid> 
+            </Grid> */}
 
+            {/*  Discussion */}
 
- */}
-
-            <Grid xs={12} className='shadow-lg over' style={{ height: '65%' }}>
+            <Grid xs={12} className='shadow-lg overflow-y-auto bg-white flex flex-row items-center rounded-xl w-full' style={{ height: '85%' }}>
                 <Message />
             </Grid>
 
-            {/* 
- formulaire
- 
- */}
-            <Grid className=' grid-flow-row auto-rows-max' xs={12} style={{ height: '15%' }}>
-                <form className='form'>
-                    <input type='textArea' className='focus:outline' multiline='2' placeholder='Votre réponse' />
+            {/*formulaire */}
 
-                    <button className='bg-blue-600 active:bg-green-700' type='submit'>
-                        <SendIcon />
-                    </button>
-                </form>
+            <Grid className=' grid-flow-row auto-rows-max fixed-bottom ' xs={12} style={{ height: '15%' }}>
+                <Champ />
             </Grid>
         </Grid>
     )
