9ce165bee9dc84082a3c0202437d117db4cf60a7
NadAziya <aitsimammarnadine@gmail.com>
2021-03-17 17:58:36 +0100

    footer design

--- src/Layout/Home/home.js
+++ src/Layout/Home/home.js
@@ -9,6 +9,7 @@ import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { ResetStateSignup } from '../../store/signup/signupReducer'
 import { ResetAuthState } from '../../store/auth/auth'
+import Footer from '../Footer/newFooter'
 
 AOS.init({
     // Global settings:
@@ -43,7 +44,7 @@ export default function Home() {
     return (
         <div>
             <div className='lg:bg-image h-screen bg-cover bg-center bg-fixed sm:bg-regal-blue '>
-                <div class='custom-shape-divider-bottom-1614867478 '>
+                <div class=' custom-shape-divider-bottom-1614867478 '>
                     <svg data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1200 120' preserveAspectRatio='none'>
                         <path
                             d='M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z'
@@ -155,6 +156,8 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+
+            <Footer />
         </div>
     )
 }
