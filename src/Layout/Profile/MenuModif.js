d4eb72227544a3d9c025cf69ca646b37464ee223
xlincw0w <khaled.khazem@yahoo.com>
2021-03-21 00:50:40 +0100

    another profile bug fix

--- src/Layout/Profile/MenuModif.js
+++ src/Layout/Profile/MenuModif.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from '@material-ui/core/Button'
 import { BsPencilSquare } from 'react-icons/bs'
 import { BsTrash } from 'react-icons/bs'
