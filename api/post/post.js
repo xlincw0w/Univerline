1181fb0a08fd81eb6dea4370e493fe1f192fde82
xlincw0w <khaled.khazem@yahoo.com>
2021-03-21 21:07:36 +0100

    feed while not colleague bug fix

--- api/post/post.js
+++ api/post/post.js
@@ -145,7 +145,7 @@ router.route('/get/post_ens/all/:id?').get(async (req, res) => {
 router.route('/get/post_ens/allfriends/:id?').get(async (req, res) => {
     const id = req.params.id
 
-    const friends = await db('collegue').select('id_user').where({ id_collegue: id })
+    const friends = await db('collegue').select('id_user').where({ id_collegue: id, confirm: true })
     const classes = await db('classe').select('*').where({ id_ens: id })
     const collegue = await db('poste')
         .select('*')
@@ -224,6 +224,7 @@ router.route('/update/post/:id?').post((req, res) => {
 //supprimer un poste
 router.route('/delete/post/:id?').delete((req, res) => {
     const id_poste = req.params.id
+
     db('poste')
         .where('id_poste', id_poste)
         .delete('*')
