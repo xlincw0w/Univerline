332acacaa509bbbcbf7702f3d0914887ece98275
xlincw0w <khaled.khazem@yahoo.com>
2021-03-29 23:56:48 +0100

    bug fix

--- src/Layout/Dashboard/Feature3.js
+++ src/Layout/Dashboard/Feature3.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { HiStatusOnline } from 'react-icons/hi'
 import { SetFriends } from '../../store/auth/auth'
-import OnlineSm from './Online/onlineSm'
+import OnlineSm from './Online/OnlineSm'
 import Axios from 'axios'
 import { constants } from '../../constants'
 
@@ -13,16 +13,16 @@ export default function Feature3() {
 
     return (
         <div className='overflow-x-scroll md:visible'>
-        <div className='flex flex-row space-x-4'>
-            {user.user_type === 'etudiant' &&
-                friends.map((elem) => {
-                    return <OnlineSm key={elem.id_user} id={elem.id_user} nom={elem.nom} prenom={elem.prenom} avatar={elem.avatar} user_type={elem.user_type} />
-                })}
-            {user.user_type === 'enseignant' &&
-                friends.map((elem) => {
-                    return <OnlineSm key={elem.id_user} id={elem.id_user} nom={elem.nom} prenom={elem.prenom} avatar={elem.avatar} user_type={elem.user_type} />
-                })}
+            <div className='flex flex-row space-x-4'>
+                {user.user_type === 'etudiant' &&
+                    friends.map((elem) => {
+                        return <OnlineSm key={elem.id_user} id={elem.id_user} nom={elem.nom} prenom={elem.prenom} avatar={elem.avatar} user_type={elem.user_type} />
+                    })}
+                {user.user_type === 'enseignant' &&
+                    friends.map((elem) => {
+                        return <OnlineSm key={elem.id_user} id={elem.id_user} nom={elem.nom} prenom={elem.prenom} avatar={elem.avatar} user_type={elem.user_type} />
+                    })}
+            </div>
         </div>
-    </div>
     )
 }
